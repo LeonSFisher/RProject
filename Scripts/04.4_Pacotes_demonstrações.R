7beeb532b02d46a47122ea0ac3eafe02f12635d2
Leandro Luz <leandroluzstat@gmail.com>
2023-05-01 14:20:44 -0300

    01/05/2023

--- Scripts/04.4_Pacotes_demonstrações.R
+++ Scripts/04.4_Pacotes_demonstrações.R
@@ -10,21 +10,20 @@
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #DEMONSTRAÇÕES
 
-#Podemos exibir demonstrações de pacotes (necessário apertar enter para alguns pacotes)
+#Podemos exibir demonstrações de pacotes. As demonstrações permitem visualizar algumas funcionalidades e estilos de uso disponíveis para estes
+#pacotes (necessário apertar ENTER para alguns pacotes).
+
+#Para isso, usamos a função demo()
 
   demo(graphics)
-  
-  demo("graphics")
-  
-#-----------------------------------------------------------------------------------------------------------------------------------------------------
-#Outras demonstrações
   
   demo(persp)
   
   demo(plotmath)
-
+  
 #----------------------------------------------------------------------------------------------------------------------------------------------------- 
 #Definindo se o tópico a ser procurado deve ser uma string de caracteres
+  
 #Procurando como nome do pacote
 
   demo(graphics, character.only = FALSE) 
@@ -36,27 +35,54 @@
   demo("graphics", character.only = FALSE)
   
   demo("graphics", character.only = TRUE)
-
+  
 #----------------------------------------------------------------------------------------------------------------------------------------------------- 
 #Definindo em que pacotes procurar a demonstração
-
-  demo("graphics", package = "graphics") 
   
-  demo("graphics", package = "base") 
-
+#Listará todas as demonstrações disponíveis
+  
+  demo(package = "graphics") 
+  
+  demo(package = "base") 
+  
 #-----------------------------------------------------------------------------------------------------------------------------------------------------  
-#Vetor de diretórios ou de 'libraries' (ou bibliotecas) onde procurar as demonstrações
-
-  demo("graphics", lib.loc = c("C:\\Program Files\\R\\R-4.2.2\\library", "E:\\GITHUB\\RProject\\Subdiretorios\\dir5")) 
-
+#O parâmetro 'verbose' também existe na função demo()
+  
+  demo(graphics, verbose = TRUE)
+  
 #-----------------------------------------------------------------------------------------------------------------------------------------------------  
 #Onde mostrar o exemplo da demonstração
+  
 #No console da aplicação   
   
   demo("graphics", type = "console")
-
+  
 #Em um arquivo html
-
+  
   demo("graphics", type = "html")
-
+  
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Interatividade na demonstração
+  
+#O parâmetro 'echo' refere-se a se queremos que sejam exibidos os inputs utilizados pela demosntração no console
+  
+#O parâmetro 'ask' diz se queremos que a demonstração espere nosso input 'ENTER' para prosseguir em cada estágio ou se deve ser executada
+#de uma única vez sem interação.
+  
+#Mostrando os inputs no console e realizando a demonstração com interatividade
+  
+  demo("graphics", echo = TRUE, ask = TRUE)
+  
+#Mostrando os inputs no console e realizando a demonstração sem interatividade  
+  
+  demo("graphics", echo = TRUE, ask = FALSE)
+  
+#Não mostrar os inputs no console e realizar a demonstração com interatividade   
+  
+  demo("graphics", echo = FALSE, ask = TRUE)
+  
+#Não mostrar os inputs no console e realizando a demonstração sem interatividade    
+  
+  demo("graphics", echo = FALSE, ask = FALSE)
+  
+#-----------------------------------------------------------------------------------------------------------------------------------------------------  
