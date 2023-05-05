e3241687cb47bfa645c1d9222387ab65300371b6
Leandro Luz <leandroluzstat@gmail.com>
2023-05-04 21:55:57 -0300

    04/05/2023

--- Scripts/09.0_Variáveis.R
+++ Scripts/09.0_Variáveis.R
@@ -42,14 +42,15 @@
   #.7tipo
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
-#Assim como palavras reservadas, devemos evitar dar aos nossos objetos,nomes de funções em R
-#Para evitar isso, digitamos o nome do objeto no prompt.Por exemplo, existe a função c() para saber se podemos criar o objeto c, fazemos:
+#Assim como palavras reservadas, devemos evitar dar aos nossos objetos, nomes de funções em R
+#Para evitar isso, digitamos o nome do objeto no prompt. Por exemplo, existe a função c() para saber se podemos criar o objeto c, fazemos:
   
   c
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------  
 #Camel case
-#Caso se queira evitar o uso de pontos e underscores nos identificadores compostos, uma alternativa é utilizar camel case
+  
+#Caso se queira evitar o uso de pontos e underscores nos identificadores compostos, uma alternativa é utilizar 'camel case'
   
   TotalDisponivel
   
@@ -69,6 +70,7 @@
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Para fazer uma atribuição a variáveis globais
+  
 #Atribuindo dessa forma, podemos acessar essas variáveis dentro do escopo das funções
   
   variavel <<- 44
@@ -76,16 +78,18 @@
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Cuidado para não confundir acrescentando espaços
   
-  x <- 5 #(Atribuição)
+  x <- 5 #(Atribuição o valor 5 à variável x)
   
-  x < -5 #(Comparação)
+  x < -5 #(Comparando se a variável x é menor do que o valor -5)
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #FORMAS DE ATRIBUIR
 
-#Em R, a atribuição é direcional, mas devemos tomar cuidado com a direção  
+#Em R, a atribuição é direcional, mas devemos tomar cuidado com a direção do operador
+  
 #Atribuição simples
+  
 #Formas de atribuição válidas
   
   a = 3 
@@ -110,6 +114,7 @@
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------  
 #Atribuição múltipla com a seta
+  
 #Formas válidas
   
   a <- b <- c <- 0
@@ -124,17 +129,21 @@
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Atribuição múltipla com o sinal de igualdade
+  
 #Forma válida
   
   a = b = c = 1 
 
 #Forma inválida
+  
 #Inverter a ordem com a seta funciona, mas com o sinal de igual, não.  
     
   1 = a = b = c
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Atribuição com a função assign()
+  
+#Atribuindo a raiz quadrada de 25, isto é, 5, à variável z
   
   assign("z", sqrt(25))
   
@@ -159,15 +168,17 @@
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #VARIÁVEIS OCULTAS
   
+#Assim como vimos que em R temos arquivos e diretórios ocultos, também podemos ter variáveis ocultas
+  
 #As variáveis ocultas não aparecem no ambiente de trabalho
   
   .valor <- 100
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#FORMA ALTERNATIVA DE DECLARAR IDENTIFICADORES
+#FORMA ALTERNATIVA DE CRIAR IDENTIFICADORES
   
-#Caso o identificador necessite espaço em branco,ele precisa ser envolvido por aspas simples ou duplas
+#Caso o identificador necessite espaço em branco, ele precisa ser envolvido por aspas simples ou duplas
   
   `turma do barulho` <- "A3"
   
@@ -175,6 +186,7 @@
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Também podemos usar apóstrofos como aspas, mas, nesse caso, não podemos usar essa forma para chamar a variável
+  
 #Se chamarmos a variável com apóstrofos como aspas simples ou duplas, eles serão confundidos com strings
   
   'numero bom' <- 7
@@ -190,5 +202,4 @@
     
   `numero bom` * `numero ruim`
   
-
 #-----------------------------------------------------------------------------------------------------------------------------------------------------  
