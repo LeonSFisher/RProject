62e1f9bb7c1a0679fa0ceb8ec693ecc8a6d43444
Leandro Luz <leandroluzstat@gmail.com>
2023-05-06 00:56:50 -0300

    06/05/2023

--- Scripts/11.1_Saída_de_dados_formatação.R
+++ Scripts/11.1_Saída_de_dados_formatação.R
@@ -8,42 +8,25 @@
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#FORMATAÇÃO DE NÚMEROS
-
-#Formatação de números 
-
-#Justificando números à esquerda
-
-  mensagem <- format(15.33 , width = 20, justify = "l")
-  
-  print(mensagem)
-
-#Justificando números ao centro 
-
-  mensagem = format(15.33 , width = 20, justify = "c")
-  
-  print(mensagem)
-
-#Justificando números à direita
-
-  mensagem = format(15.33 , width = 20, justify = "r")
-  
-  print(mensagem) 
-
-#-----------------------------------------------------------------------------------------------------------------------------------------------------  
-#Definindo o número de dígitos significativos para numeric e complex
-
-  format(3.345456, width = 20, justify = "c", digits = 3)
-
-#-----------------------------------------------------------------------------------------------------------------------------------------------------  
-#Definindo se quer em notação científica
-
-  format(3.345456, width = 20, justify = "c", scientific = TRUE) 
-
-#-----------------------------------------------------------------------------------------------------------------------------------------------------
-#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #FORMATAÇÃO DE STRINGS
-
+  
+#Com o parâmetro 'width' definimos o tamanho do espaço entre as aspas
+  
+#Quando utilizamos um tamanho menor ou igual ao da mensagem a ser exibida, há um ajuste automático
+  
+  format("Olá, mundo!", width = 2)
+  
+  format("Olá, mundo!", width = 11)
+  
+#Quando definimos um tamanho maior que o da string a exibir,percebemos a diferença
+  
+  format("Olá, mundo!", width = 20)
+  
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Com o parâmetro 'justify' podemos justificar o texto
+  
+#Esse efeito só é percebido para um tamanho maior do que o da string, obviamente
+  
 #Justificando texto à esquerda (left)
 
   format("Olá, mundo!", width = 20, justify = "l")
@@ -59,15 +42,37 @@
 #Sem justificação (none)  
 
   format("Olá, mundo!", width = 20, justify = "n")
-
+  
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Maiúsculas e minúsculas  
-#Maiúsculas
-
+  
+#Para colocar uma string em maiúsculas
+  
   toupper("Olá, mundo!")
+  
+#Para colocar uma string em minúsculas
+  
+  tolower("Olá, mundo!")  
 
-#Minúsculas
-
-  tolower("Olá, mundo!")
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Formatação de números
+  
+#Perceba que os números são sempre justificados à direita
+  
+  mensagem = format(15.33 , width = 20, justify = "l")
+  
+  mensagem = format(15.33 , width = 20, justify = "c")
+  
+  mensagem = format(15.33 , width = 20, justify = "r")
+  
+#-----------------------------------------------------------------------------------------------------------------------------------------------------  
+#Definindo o número de dígitos significativos (para numeric e complex)
+  
+  format(3.345456, width = 20, digits = 3)
+  
+#-----------------------------------------------------------------------------------------------------------------------------------------------------  
+#Definindo se quer a exibição em notação científica
+  
+  format(3.345456, width = 20, scientific = TRUE) 
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
--- Scripts/12.0_Sequências_de_escape.R
+++ Scripts/12.0_Sequências_de_escape.R
@@ -1,7 +1,7 @@
 ######################################################################################################################################################
 ######################################################################################################################################################
 
-#SEQUÊNCIAS DE ESCAPE 
+#SEQUÊNCIAS DE ESCAPE
 
 ######################################################################################################################################################
 ######################################################################################################################################################
@@ -10,31 +10,36 @@
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #SEQUÊNCIAS DE ESCAPE COMUNS
 
-#Imprimingo uma aspa: sequência \'
+#Imprimingo aspas: sequência \'
+
 #Com a sequência \' não haverá conflito entre as aspas da string e o apóstrofo da palavra d'água
 
   cat('Marca d\'água')
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------  
 #Imprimindo uma barra invertida 'backslash' 
+
 #Uma barra invertida pode ser mal interpretada como uma sequência de escape. Para representar a barra invertida, usamos \\
 
   cat("E:\\GITHUB\\RProject\\Subdiretorios\\dir5\\bizu.txt")  
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------- 
 #Pulando linhas
+
 #O caractere de nova linha \n segmenta a string em duas
 
   cat("Primeira linha\nSegunda linha")
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------  
 #Inserindo tabulação
+
 #As tabulações são recuos dados toda vez que se encontra o caractere \t (tab)
 
   cat("Casa\tCarro\tLivro\tBicicleta")
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------- 
 #Backspace
+
 #Para cada caractere \b um salto para trás na string é dado (recuo do prompt)
 
   cat("Nova frase\bcachorro")
@@ -43,18 +48,17 @@
   
 #Uma forma mais útil de usar o \b é para remover o espaço antes da vírgula
   
+  cat("O valor é:", 155, ", e nada menos!")
+  
   cat("O valor é:", 155, "\b, e nada menos!")
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------  
-#Retrocendo o prompt
+#Retrocedendo o prompt
+  
 #O carcatere \r 'carriage return' é semelhante ao \b, mas leva o prompt de volta ao começo da string
-#Ao se escrever outra frase depois de \r, esta substituirá os caracteres que já foram escritos a partir do começo
+#Ao se escrever outra frase depois de \r, esta substituirá os caracteres que já foram escritos a partir do começo da linha
 #Os caracteres que não foram sobrescritos serão preservados
 
   cat("Voltando\rLeandro")
-
-
-  
-  
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
