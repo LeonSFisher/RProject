a02dc0dc1331bbb59e1c16dc322cfe49652a0364
Leandro Luz <leandroluzstat@gmail.com>
2023-05-07 22:51:26 -0300

    07/05/2023

--- Scripts/15.1_Vetores_nomeando.R
+++ Scripts/15.1_Vetores_nomeando.R
@@ -12,54 +12,71 @@
 
 #Criando um vetor para nomear
   
-  vetor1 <- c(4, 3, 1)
+  vetor <- c(4, 3, 1)
   
 #Criando nomes para o vetor
   
-  nomesvetor1 <- c("casas", "carros", "motos")
+  nomesvetor <- c("casas", "carros", "motos")
 
 #Atribuição dos nomes
 
-  names(vetor1) <- nomesvetor1
+  names(vetor) <- nomesvetor
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
-#Se adicionarmos mais um valor, os nome da nova casa do vetor fica NA
+#Adionando valores a um vetor
+  
+#Se adicionarmos mais um valor, a nova posição do vetor fica sem nome
 
-  vetor1[4] <- 5
+  vetor[4] <- 5
 
-#Corrigimos adicionando um novo nome
+#Corrigimos isso adicionando um novo nome
 
-  nomesvetor1[4] <- "tipo" 
+  nomesvetor[4] <- "aviões" 
   
 #Reatribuindo os nomes
   
-  names(vetor1) <- nomesvetor1
+  names(vetor) <- nomesvetor
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------  
 #Menos nomes do que casas faz com que as casas restantes do vetor fiquem nomeadas como NA
 
-  vetor1 <- c(2, 5, 7, 5, 7, 7)
+  vetor <- c(2, 5, 7, 5, 7, 7)
   
-  nomesvetor1 <- c("asa", "cauda", "bico")
+  nomesvetor <- c("asa", "cauda", "bico")
   
-  names(vetor1) <- nomesvetor1
+  names(vetor) <- nomesvetor
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------  
 #Mais nomes do que casas não são permitidos
 
-  vetor1 <- c(2, 5, 7)
+  vetor <- c(2, 5, 7)
   
-  nomesvetor1 <- c("asa", "cauda", "bico", "penas", "olhos")
+  nomesvetor <- c("asa", "cauda", "bico", "penas", "olhos")
   
-  names(vetor1) <- nomesvetor1
+  names(vetor) <- nomesvetor
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Removendo nomes do vetor
   
-  vetor1 <- as.vector(vetor1)
+#Para remover os nomes de um vetor, não basta remover o vetor de nomes que lhe fora atribuído, pois uma vez que isso seja feito, apenas o vetor de
+#nomes que foi criado é apagado. Os nomes persistem no vetor nomeado como atributo.
+  
+  vetor <- c(2, 5, 7)
+  
+  nomesvetor <- c("asa", "cauda", "bico")
+  
+  names(vetor) <- nomesvetor
+  
+  rm(nomesvetor)
+  
+  vetor
+  
+#Para eliminar os nomes de um vetor, reatribuímos a ele seus próprios valores com a função as.vector()
+  
+  vetor <- as.vector(vetor)
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
-  #Nomes dos vetores
+#Herança dos nomes de vetores combinados
   
   vetor1 <- c(1, 2, 3)
   
@@ -69,6 +86,17 @@
   
   names(vetor2) <- c("barcos", "castelos", "lojas")
   
-  vetor3 <- c(vetor1, vetor2, use.names = FALSE)
+  vetor <- c(vetor1, vetor2)
+  
+#Percebemos acima, que uma vez que tenhamos concatenado vetores, o vetor resultante herda os nomes dos vetores que o compõe nas posições respectivas.
+#Podemos definir se esse comportamento deve prevalecer ou não. Para isso, utilizamos o parâmetro 'use.names'. 
+  
+#Explicitando o parâmetro com seu valor padrão, TRUE, obtemoso mesmo efeito anterior  
+  
+  vetor <- c(vetor1, vetor2, use.names = TRUE)
+  
+#Usando o valor FALSE, o vetor resultante da concatenação não herda os nomes dos vetores que o compõe
+  
+  vetor <- c(vetor1, vetor2, use.names = FALSE)  
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
