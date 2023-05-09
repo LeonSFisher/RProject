2b3e1d05dac49edecf035550c121f4837efcd3c7
Leandro Luz <leandroluzstat@gmail.com>
2023-05-09 01:29:57 -0300

    09/05/2023

--- Scripts/16.2_Matrizes_nomeando.R
+++ Scripts/16.2_Matrizes_nomeando.R
@@ -22,32 +22,39 @@
 
   vetor =  c(1.78, 1.69, 1.88, 65, 70, 80, 17, 35, 44)
 
-#Criação de matriz nomeada
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Criação de uma matriz nomeada internamente
+  
+#Para nomear a matriz utilizamos o parâmetro 'dimnames' que recebe uma lista com o que serão os nomes das linhas e colunas, respectivamente
 
   matriz <- matrix(vetor, nrow = 3, ncol = 3, byrow = FALSE, dimnames = list(linhas, colunas))
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
-#Outra opção (1) - Em vez de usar um parâmetro dimnames da função matrix(), usar as funções rownames() e colnames()
+#Criação de uma matriz nomeada externamente
+  
+#Em vez de usar um parâmetro dimnames da função matrix(), usar as funções rownames() e colnames()
   
   matriz <- matrix(vetor, nrow = 3, ncol = 3, byrow = FALSE)
 
   rownames(matriz) <- c("alberto", "francisco", "helder")
   
   colnames(matriz) <- c("altura", "peso", "idade")
-
-#-----------------------------------------------------------------------------------------------------------------------------------------------------
-#Outra opção (2) - Podemos também nomear uma matriz com uma lista  
-
-  dimnames(matriz) <- list(c("alberto", "francisco", "helder"), c("altura", "peso", "idade"))
-
-#-----------------------------------------------------------------------------------------------------------------------------------------------------
-#Outra opção (3) - Usar uma operação com vetores usando paste()
-
+  
+#Outra opção é usar uma operação com vetores usando paste() para nomear matrizes cujas linhas e/ou colunas possuam nomes que obedeçam a certo padrão
+  
   rownames(matriz) <- paste("linha", c(1, 2, 3), sep = "_")
   
   colnames(matriz) <- paste("coluna", c(1, 2, 3), sep = "_")  
-  
+
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Criação de uma matriz nomeada com uma função de atributo
   
+#Podemos também nomear uma matriz com uma lista com os nomes
+
+  dimnames(matriz) <- list(c("alberto", "francisco", "helder"), c("altura", "peso", "idade"))
   
-  #names em diag
+#Ou, equivalentemente
+  
+  dimnames(matriz) <- list(linhas, colunas)
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
