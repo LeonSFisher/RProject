523182cb7622fa5879f8ca675b16350222898785
Leandro Luz <leandroluzstat@gmail.com>
2023-05-12 21:58:19 -0300

    12/05/2023

--- Scripts/_17.0_Fatores_criação.R
+++ Scripts/_17.0_Fatores_criação.R
@@ -10,20 +10,28 @@
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #CRIAÇÃO DE NÍVEIS
 
+#Níveis são os valores que uma variável categórica pode assumir. Eles são justamente os valores que são registrados quando da coleta dos dados.
+
 #Gerando níveis para o fator
 
 #CE - Ceará // PE - Pernambuco // SP - São Paulo // AM - Amazonas
-#Vetor de níveis - como as variáveis serão marcadas na pesquisa (devem ser como os valores de input)
 
   niveis <- c("PE", "CE", "SP", "AM")
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
-#Vetor de rótulos - o que os níveis significam (mesma ordem que os níveis)
+#Vetor de rótulos
+  
+#Os que os níveis significam. Servem como apelidos para facilitar a visualização pois muitas vezes os valores de uma variável categórica são apenas 
+#códigos representando o que elas realmente significam.
+  
+#No vetor de rótulos, cada rótulo deve ficar na posição homóloga ao vetor de níveis
   
   rotulos <- c("Pernambuco", "Ceará", "São Paulo", "Amazonas")
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #vetor de valores - os dados coletados
+  
+#Aqui são os valores coletados propriamente ditos
   
   valores <- c("PE", "CE", "SP", "SP", "PE", "SP", "AM", "AM", "PE", "SP", "CE")
 
@@ -32,7 +40,8 @@
 #CRIAÇÃO DE FATORES
 
 #Resultado da rotulação - cada valor na pesquisa (vetor valores) terá seu nível substituído pelo seu rótulo respectivo
-#O parâmetro ordered como FALSE considerará que não existe relação de ordem por detrás (para categóricas ordinais)
+  
+#O parâmetro ordered como FALSE considerará que não existe relação de ordem por detrás dos valores (para categóricas ordinais)
 
   resultado <- factor(valores, labels = rotulos, ordered = FALSE, levels = niveis)
 
@@ -42,7 +51,7 @@
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#VERIFICANDO NÍVEIS DE UM FATOR
+#INFORMAÇÕES DE FATORES
   
 #Saber o número de níveis
   
@@ -73,11 +82,12 @@
 #EXCLUSÃO DE NÍVEIS
   
 #Excluindo níveis de um vetor de levels quando criar um conjunto de níveis a partir de um já existente  
+  
 #Necessário que os rótulos sejam do mesmo tamanho que o vetor de níveis depois dos valores excluídos
   
-  rotulos <- c("Pernambuco", "Ceará")
+  novos_rotulos <- c("Pernambuco", "Ceará")
   
-  resultado <- factor(valores, labels = rotulos, ordered = TRUE, levels = niveis, exclude = c("SP", "AM"))
+  resultado <- factor(valores, labels = novos_rotulos, ordered = TRUE, levels = niveis, exclude = c("SP", "AM"))
   
 #Com a exclusão, os valores cujos níveis correspondestes foram excluídos perderão também o rótulo associaso e serão rotulados como NA
   
