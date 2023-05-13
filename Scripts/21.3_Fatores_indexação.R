a4f5ffa12b1b5dfc480826900960e552d2f193b9
Leandro Luz <leandroluzstat@gmail.com>
2023-05-12 23:05:32 -0300

    12/05/2023

--- Scripts/21.3_Fatores_indexação.R
+++ Scripts/21.3_Fatores_indexação.R
@@ -8,46 +8,65 @@
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#ALTERANDO ELEMENTOS DOS NÍVEIS DE UM FATOR
+#INDEXAÇÃO DE FATORES
+  
+  niveis <- c("PE", "CE", "SP", "AM")
+  
+  rotulos <- c("Pernambuco", "Ceará", "São Paulo", "Amazonas")
+  
+  valores <- c("PE", "CE", "SP", "SP", "PE", "SP", "AM", "AM", "PE", "SP", "CE")
+  
+  resultado <- factor(valores, labels = rotulos, ordered = FALSE, levels = niveis)
+  
+  resultado <- factor(valores, labels = rotulos, ordered = TRUE, levels = niveis)
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Acessando um elemento do vetor de fatores
 
-  dados1[3]
+  resultado[3]
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------  
 #Alterando com um valor que está nos níveis
+  
+#A alteração de um valor de um vetor de fatores pode ocorrer normalmente se o novo valor estiver no vetor de níveis
 
-  dados1[3] <- 3
+  resultado[3] <- "Ceará"
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------- 
 #Tentando alterar com um valor que não está nos níveis (gera um NA na posição)
 
-  dados1[3] <- 9
+  resultado[3] <- "DF"
 
-#----------------------------------------------------------------------------------------------------------------------------------------------------- 
-#Adicionando um novo level ao vetor de factors
+#Como pode ser visto, se o novo valor não estiver no vetor de níveis, ele não pode ser introduzido no lugar de um valor existente no vetor de fatores.
+#Para isso, é preciso acrescentar o novo valor no vetor de níveis e depois efetuar a substituição no vetor de fatores.
+  
+#Acrescentando um novo vaolor no vetor de níveis
 
-  levels(dados1) <- c(levels(dados1), 9)
+  levels(resultado) <- c(levels(resultado), "DF")
   
-  dados1
+  resultado
   
-  dados1[3] <- 9
+  resultado[3] <- "DF"
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #COMPARANDO FATORES
   
-#Classes ordinais
+#Podemos indexar valores para efetuar comparações
+  
 #Para o caso em que ordered = TRUE faz sentido perguntar o seguinte:
   
   resultado[1] < resultado[2]
+  
   resultado[1] > resultado[4]
+  
   resultado[1] == resultado[1]
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Para o caso em que ordered = FALSE não faz sentido perguntar o seguinte:
   
   resultado[1] < resultado[2]
+  
   resultado[1] > resultado[4]
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
