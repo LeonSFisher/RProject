8708817f352316ace88ed23a308a38894d23401a
Leandro Luz <leandroluzstat@gmail.com>
2023-05-15 15:12:08 -0300

    15/05/2023

--- Scripts/_x.R
+++ Scripts/_x.R
@@ -1,71 +1,23 @@
-#salvando objetos
-save(objetos, file = "arquivo.Rdata")
-
-#carregando arquivos
-load(file = "arquivo.Rdata")
-
-#pacotes de dados disponíveis no R
-#descobrir pacotes
-data()
-data("airquality")
-
-#basesde dados de um pacote específico
-data(package = "dplyr")
-
-#FUNÇÕES PARA REALIZAR OPERAÇÕES DE CASTING
-#converte caractere para inteiro ASCII
-charToRaw()
-
-#Mostra a estrutura interna de um objeto
-str()
-
-#verificar se é um vetor
-unique()
-
-#mostra um resumo de um conjunto de dados
-summary()
-
-#operador in
-vetor4 = 3:21
-
-x = 0
-y = 7
-
-print(x %in% vetor4)
-print(y %in% vetor4)
-
-#BUSCA
-dev.off()
-
-#data
-date()
-
-#Verificando instalação
-
-any(grep('xlsx', install.packages()))
-
-dev.copy()
-
-attributes()
-
-Sys.time()
-
-format()
-
-format(Sys.time(), "%H")
-
-is.atomic()
-
-class()
-
-rep()
-
-sweep()
-
-agrep()
-
-grep()
-
 aggregate()
-
+agrep()
+any(grep('xlsx', install.packages()))
+attributes()
 by()
+charToRaw()
+class()
+data("airquality")
+date()
+dev.off()
+dev.copy()
+format(Sys.time(), "%H")
+grep()
+is.atomic()
+load(file = "arquivo.Rdata")
+print(x %in% vetor4)
+rep()
+save(objetos, file = "arquivo.Rdata")
+str()
+summary()
+sweep()
+Sys.time()
+unique()
