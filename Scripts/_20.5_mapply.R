136b7d99e8464b942cbce1039cf22880a0daa186
Leandro Luz <leandroluzstat@gmail.com>
2023-05-15 22:10:16 -0300

    15/05/2023

--- Scripts/_20.5_mapply.R
+++ Scripts/_20.5_mapply.R
@@ -10,17 +10,48 @@
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #mapply()  
 
-#A função mapply é uma versão multivariada da função lapply e sapply. 
-#As funções lapply e sapply atuam somente sobre os elementos de uma única lista. 
-#É aplicada sobre o primeiro elemento de cada um dos argumentos, em seguida ao segundo elemento, seguindo ao terceiro, e assim por diante. 
-#Os argumentos podem ser listas ou vetores.
+#A função mapply é uma versão multivariada da função lapply e sapply. As funções lapply e sapply atuam somente sobre os elementos de uma única lista. 
+#mapply() é aplicada sobre o primeiro elemento de cada um dos argumentos, em seguida ao segundo elemento, seguindo ao terceiro, e assim por diante.
 
-  L1 <- list(a = LETTERS[c(4,6,12,6)], b = LETTERS[c(1,5,21,1)])
-  
-  L2 <- list(c = LETTERS[c(4,14,22,20)], d = LETTERS[c(15,15,1,15)])
-  
-#Une as letras, contidas na mesma posição de cada vetor da lista L1 e L2 para escrever as palavras DADO, FENO,LUVA e FATO
-  
-  mapply(paste, L1$a, L1$b, L2$c, L2$d)
+#Em geral, cada elemento homólogo de cada input é utilizado como argumento da função a ser applicada na sua correspondente ordem.
 
+#Unindo nomes e sobrenomes. 
+
+#A função paste() une tantas strings quantas forem dadas de modo que aqui podemos passar tantas listas ou vetores quanto quisermos. As strings das
+#posições homólogas são unidas para formar uma só.
+
+  lista1 <- list(nomes=c("leandro", "marcos","paulo"))
+  
+  lista2 <- list(sobrenomes=c("luz", "silva","fernandes"))
+  
+  mapply(paste, lista1, lista2)
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Calculando valores máximo e mínimos
+  
+#As funções max() e min() retornam o valor máximo e mínimo, respectivamente, de um conjunto de valores de tamanho arbitrário. Com isso, com elas 
+#podemos passar tantos argumentos pudermos. As funções serão aplicadas a cada conjunto de valores homólogos.
+  
+  vetor1 <- c(-1, 7, 7, 4, -6)
+  
+  vetor2 <- c(-7, 17, 5, 1, -1)
+  
+  vetor3 <- c(11, 2, 87, -41, 8)
+  
+  mapply(max, vetor1, vetor2, vetor3)
+  
+  mapply(min, vetor1, vetor2, vetor3)
+  
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Algumas operações podem retornar matrizes
+  
+  mapply(rep, 1:5, times=6)
+  
+  mapply(rep, 1:5, 6:8, times=6)
+  
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+  
+  
+  mapply(sqrt, vetor1, vetor2, vetor3)
+  
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
