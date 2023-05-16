c5a91dfcb3d4a39e0151c24cbc7a60c9c24d98dc
Leandro Luz <leandroluzstat@gmail.com>
2023-05-15 22:28:26 -0300

    15/05/2023

--- Scripts/24.4_Família_Apply_mapply.R
+++ Scripts/24.4_Família_Apply_mapply.R
@@ -32,11 +32,11 @@
 #As funções max() e min() retornam o valor máximo e mínimo, respectivamente, de um conjunto de valores de tamanho arbitrário. Com isso, com elas 
 #podemos passar tantos argumentos pudermos. As funções serão aplicadas a cada conjunto de valores homólogos.
   
-  vetor1 <- c(-1, 7, 7, 4, -6)
+  vetor1 <- c(1, 7, 7, 4, 6)
   
-  vetor2 <- c(-7, 17, 5, 1, -1)
+  vetor2 <- c(7, 17, 5, 1, 1)
   
-  vetor3 <- c(11, 2, 87, -41, 8)
+  vetor3 <- c(11, 2, 87, 41, 8)
   
   mapply(max, vetor1, vetor2, vetor3)
   
@@ -45,12 +45,17 @@
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Algumas operações podem retornar matrizes
   
+#A função rep() replica um valor passado como parâmetro o número de vezes passado pelo segundo parâmetro. Assim, ao passarmos dois vetores para a
+#função mapply() com a função rep(), as posições homólogas dos dois vetores formam pares de valores a serem interpretados como os parâmetros 
+#respectivos de que rep() necessita.
+  
   mapply(rep, 1:5, times=6)
   
   mapply(rep, 1:5, 6:8, times=6)
   
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
-  
+#Perceba que utilizar uma função na qual não faça sentido utilização de valores homólogos não funciona com mapply(). A função sqr() utiliza apenas um
+#valor como entrada de dados e, portanto, não faz sentido neste contexto.
   
   mapply(sqrt, vetor1, vetor2, vetor3)
   
