bedd23c4701e91cc81bc4ff487860cd2a37decf2
Leandro Luz <leandroluzstat@gmail.com>
2023-05-21 15:04:12 -0300

    21/05/2023

--- Scripts/15.6.1_Vetores_de_caracteres_sub()_e_gsub().R
+++ Scripts/15.6.1_Vetores_de_caracteres_sub()_e_gsub().R
@@ -10,20 +10,18 @@
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #sub()
 
-  vetor_carac <- c("marcos", "marcela", "marcondes", "jucelino", "rodrigo","marcia", "leonardo", "francisco", "MARTINS", "jonas", "VaLdEmAr")
+  texto <- c("BrXil, oficialmente República Federativa do BrXil, é o maior país da América do Sul. É um país de extremos")
   
 #A função sub() serve para substituir um determinado valor que combine com o padrão, mas a substituição será feita apenas na primeira ocorrência.
   
+  sub(pattern = "X", replacement = "as", texto, ignore.case = FALSE)
   
+#Para substituir todas as ocorrências, utilizamos a função gsub()
   
+  gsub(pattern = "X", replacement = "as", texto, ignore.case = FALSE)  
   
+#Caso utilizássemos ignore.case como sendo TRUE, haveria substituição indevida na palavra 'extremos' que possui um 'x' minúsculo.
   
+  gsub(pattern = "X", replacement = "as", texto, ignore.case = TRUE) 
   
-  
-  
-  
-  
-  
-  
-  
-  
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
