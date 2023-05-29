2f708647788c176d378d56d51380d537b649584f
Leandro Luz <leandroluzstat@gmail.com>
2023-05-29 12:32:25 -0300

    29/05/2023

---
+++ Scripts/Pronto/00.0_Apresentação_história_e_vantagens.R
@@ -0,0 +1,70 @@
+######################################################################################################################################################
+######################################################################################################################################################
+
+#APRESENTAÇÃO - HISTÓRIA DO R
+
+######################################################################################################################################################
+######################################################################################################################################################
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#HISTÓRIA DO R
+
+#O R é um dialeto da linguagem S que foi originalmente desenvolvida por John Chambers na Bell Telephone Laboratories que fazia parte da AT&T Corp.
+#R também possui influência da linguagem Scheme. Assim, é muito similar em aparência com S, mas sua implementação e semântica é derivada de Scheme.
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#John Chambers assim resumiu a linguagem S: “We wanted users to be able to begin in an interactive environment, where they did not consciously think 
+#of themselves as programming. Then as their needs became clearer and their sophistication increased, they should be able to slide gradually into 
+#programming, when the language and system aspects would become more important”.
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Um dos problemas de S é que ela estava limitada ao pacote comercial S-PLUS. Assim, R começou a ser desenvolvida por dois pesquisadores chamados 
+#Robert Gentleman e Ross Ihaka (R & R) na Universidade de Aukland, Nova Zelândia em 1991.
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#O primeiro relato da distribuição do R foi em 1993 no StatLib, um sistema de distribuição de softwares estatísticos e com o incentivo de um dos 
+#primeiros usuários deste programa, Martin Mächler (do Instituto Federal de Tecnologia de Zurique, na Suíça), “R & R”, em 1995, lançaram o código 
+#fonte do R, disponível por ftp.
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Em 1997, foi formado um grupo de profissionais que têm acesso ao código fonte do R, possibilitando, assim, a atualização mais rápida do software. 
+#Desde então, o R vem ganhando cada vez mais adeptos em todo o mundo.
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Para citar a utilização do R em trabalhos publicados e dar o reconhecimento à equipe de desenvolvimento, podemos encontrar a forma de citar no R com
+#a ajuda do seguinte comando:
+
+  citation()
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Para obter informações importantes sobre o R, sistema operacional etc., use o comando:
+
+  R.version
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#VANTAGENS DO R
+
+#Gratuito (Licença Pública Geral (GNU) da Free Software Foundation).
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Grande comunidade auxiliando o aprendizado de novos usuários.
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Facilidade em se encontrar material escrito ou digital.
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Multiplataforma - Pode ser usado nos sistemas operacionais Windows, Linux e Mac OS.
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Amplamente utilizado no meio acadêmico e se popularizando no mercado privado.
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Possui uma vastidão de pacotes que facilitam as tarefas e dispões de ferramentas customizadas para quase todas as áreas de estudo.
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
--- Scripts/Pronto/00.1_Apresentação_boas_práticas_de_programação.R
+++ Scripts/Pronto/00.1_Apresentação_boas_práticas_de_programação.R
@@ -13,36 +13,38 @@
 #Utilizar sempre comentários. 
 
 #Os comentários são feitos com o símbolo de '#'. Ao chegar um símbolo de '#', o R ignorará aquela linha completamente e prosseguirá com a execução na 
-#linha seguinte. Os comentários podem ser utilizados para:
-
-#--Explicar seções do código. Os comentários são executados após um símbolo de '#' e vão até o fim da linha.
-#--Anular comandos durante a fase de testes do script.
+#linha seguinte. Os comentários podem ser utilizados para explicar seções do código e para anular comandos durante a fase de testes do script.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Utilizar, quando possível, indentações
 
-#Indentações servem para promever clareza no código. Trata-se do uso de linhas em branco para espaçar os comandos verticalmente e tabulações para
-#espaçá-los horizontalmente. isso permite identificar apenas olhando o escopo de funções, blocos de códigos, loopings, condicionais etc., gerando
-#organização para o código e, consequentemente, facilitando seu entendimento e manutenção.
+#Indentações servem para promever clareza no código. Trata-se do uso de tabulações de uma linha para outra afim de espaçar o código horizontalmente e 
+#deixa-lo mais legível. Às vezes, é uma boa prática deixar linhas de código em branco entre linhas de código sucessivas. Isso nos permite identificar,
+#apenas olhando, o escopo de funções, loopings, condicionais etc., gerando organização para o código e, consequentemente, facilitar o entendimento e 
+#manutenção do mesmo. Para a realização de identação automática, selecionamos o trecho de código que desejamos identar e pressionamos o comando de 
+#atalho Ctrl + i.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Limpeza do console
 
-#Quando possível, limpar o console para despoluir a visualização das saídas. (ctrl + l).
+#Quase sempre, o console ficará lotado de saídas de códigos. Com isso pode começar a ficar confusa a identificação e visualização de resultados que
+#foram obtidos anteriormente. Para isso, quando possível, podemos limpar o console para despoluir a visualização da saída com o atalho (ctrl + l).
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Nomeando objetos de forma inteligente
 
-#Nomear objetos efêmeros (aqueles usados dentro de uma mesma seção) com nomes curtos para facilitar. Nomear objetos persistentes (aqueles usados 
-#entre seções distintas), ou seja, possuem utilização frequente, com nomes significativos que transmitam sua utilidade.
+#Os objetos que utilizamos em R devem possuir nomes significativos para que nós (e outros) possamos entender o que está sendo feito depois de muito
+#tempo longe do código ou quando pegarmos códigos de terceiros. Para isso, adotaremos a seguinte prática: nomear objetos efêmeros (aqueles usados 
+#dentro de uma mesma seção) com nomes curtos para facilitar, e nomear objetos persistentes (aqueles usados entre seções distintas), ou seja, possuem
+#utilização frequente, com nomes significativos que transmitam sua utilidade.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Bom uso de recursos
 
-#Nunca deixar de descarregar pacotes depois de utilizados para evitar conflitos de funções de mesmo nome oriundas de outros pacotes e redução de 
-#desempenho por sobrecarga da memória.
+#Nunca deixar de descarregar pacotes de funções depois de utilizados para evitar conflitos com funções que tenham o nome,mas que sejam oriundas de 
+#outros pacotes e também por questões de redução de desempenho por sobrecarga da memória.
 
-#Utilzar ao máximo funções otimizadas para vetorização objetivando ganho de eficiência.
+#Também é conveniente Utilzar ao máximo funções otimizadas que utilizam a técnica da vetorização para ganho de eficiência.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Organização de arquivos
