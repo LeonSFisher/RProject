9f63d4a84aa8bd6219baba043e6f47247551a11b
Leandro Luz <leandroluzstat@gmail.com>
2023-06-13 21:32:35 -0300

    13/06/2023

--- Checked/00.1_Apresentação_vantagens_do_R.R
+++ Checked/00.1_Apresentação_vantagens_do_R.R
@@ -14,7 +14,8 @@
 
 #Gratuito (Licença Pública Geral (GNU) da Free Software Foundation).
 
-#Não énecessário pagar nenhum tipo de royalty para utilizar o R e nem comprar uma licença.
+#Não é necessário pagar nenhum tipo de royalty para utilizar o R e nem comprar uma licença. O RStudio possui versões pagas, sobretudo para empresas,
+#mas, para a grande maioria das utilizações que possamos fazer dele, a vesão gratuita é suficiente.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Grande comunidade auxiliando o aprendizado de novos usuários.
@@ -28,13 +29,30 @@
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Facilidade em se encontrar material escrito ou digital.
 
+#Há um sem número de livros, apostilas, sites, vídeos tutoriais e cursos pagos ou gratuitos disponíveis na internet e que fornecem informações muito
+#interessantes e instrutivas para a utilização do R. Não só isso, não é raro encontrar textos dedicados de uma determinada área de aplicação do R
+#como, por exemplo, IA, engenharia florestal, ciências sociais (e outras) que dispõem de ensinamentos sobre o R voltado para estudantes, curiosos e 
+#profissionais daquela área de estudo.
+
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Multiplataforma - Pode ser usado nos sistemas operacionais Windows, Linux e Mac OS.
+
+#Não é difícil encontrar plataformas onde o R possa ser executado. Dessa forma, podemos construir nossos scripts numa plataforma e executá-los em
+#outra obtendo grande portabilidade do nosso trabalho.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Amplamente utilizado no meio acadêmico e se popularizando no mercado privado.
 
+#Embora python ainda seja a linguagem mais utilizada em ci6encia de dados, fato é que o R caiu nas graças dos estatísticos e a sua difuão para todos
+#os setores que necessitem de análise de dados é irremediável. Isso não é de se espantar ao consideramos o fato de que o R nasce justamente como uma
+#linguagem voltada para análises estatísticas eficientes e documentáveis.
+
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Possui uma vastidão de pacotes que facilitam as tarefas e dispões de ferramentas customizadas para quase todas as áreas de estudo.
+
+#A programação orientada a objetos introduziu a figura da reutilização efiente de código. Assim, bibliotecas e pacotes de funções que são amplamente 
+#utilizados tornaram-se confiáveis por terem passado pelo crivo de milhares de utilizadores de diversas áreas e resistido ao tempo como ferramental
+#de utilidade. Recomenda-se, sempre que possível, utilizar pacotes para evitar retrabalho, erros que já tenham sido neles mitigados e agilizar a 
+#construção de scripts.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
--- Checked/01.0_Boas_práticas_de_programação.R
+++ Checked/01.0_Boas_práticas_de_programação.R
@@ -1,7 +1,7 @@
 ######################################################################################################################################################
 ######################################################################################################################################################
 
-#APRESENTAÇÃO - BOAS PRÁTICAS DE PROGRAMAÇÃO
+#BOAS PRÁTICAS DE PROGRAMAÇÃO
 
 ######################################################################################################################################################
 ######################################################################################################################################################
@@ -14,8 +14,12 @@
 
 #Utilizar sempre comentários. 
 
-#Os comentários são feitos com o símbolo de '#'. Ao chegar um símbolo de '#', o R ignorará aquela linha completamente e prosseguirá com a execução na 
-#linha seguinte. Os comentários podem ser utilizados para explicar seções do código e para anular comandos durante a fase de testes do script.
+#Os comentários são feitos com o símbolo de '#'. Ao chegar a um símbolo de '#', o R ignorará aquela linha completamente e prosseguirá com a execução 
+#na linha seguinte. Os comentários podem ser utilizados para explicar seções do código e para anular comandos durante a fase de testes do script. É
+#bom lembrar que, durante nossa vida, trabalharemos muitos scripts e raramente nos lembraremos do que nos motivou a escrever um código de determinada
+#forma quando voltarmos a ele depois de muito tempo. Com isso, os comentários nos ajudam a rememorar o que está sendo feito ou mesmo justificar o
+#motivo de o código ser do jeito que é. Também colegas poderão trabalhar num código iniciado por você e eles precisarão entender todo o processamento
+#que o script faz. Com bons comentários explicativos, tudo fica facilitado.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Utilizar, quando possível, indentações
@@ -23,14 +27,17 @@
 #Indentações servem para promever clareza no código. Trata-se do uso de tabulações de uma linha para outra afim de espaçar o código horizontalmente e 
 #deixa-lo mais legível. Às vezes, é uma boa prática deixar linhas de código em branco entre linhas de código sucessivas. Isso nos permite identificar,
 #apenas olhando, o escopo de funções, loopings, condicionais etc., gerando organização para o código e, consequentemente, facilitar o entendimento e 
-#manutenção do mesmo. Para a realização de identação automática, selecionamos o trecho de código que desejamos identar e pressionamos o comando de 
-#atalho Ctrl + i.
+#manutenção do mesmo tanto por nós como por colegas. Para a realização de identação automática, selecionamos o trecho de código que desejamos identar 
+#e pressionamos o comando de atalho Ctrl + i.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Limpeza do console
 
 #Quase sempre, o console ficará lotado de saídas de códigos. Com isso pode começar a ficar confusa a identificação e visualização de resultados que
 #foram obtidos anteriormente. Para isso, quando possível, podemos limpar o console para despoluir a visualização da saída com o atalho (ctrl + l).
+#Muitas funções do R possuem um parâmetro chamado 'quietly' que evita que a função fique retornando dados desnecessários no console. Outras funções
+#possui um par6ametro chamado 'echo' que permite determinar se os dados devem ser exibidos após o processamento dos dados. Utilizar esses parâmetros
+#de forma inteligente pode fazer o processamento de nosso script retornar de forma limpa e sem muita confusão desnecessária.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Nomeando objetos de forma inteligente
@@ -43,27 +50,35 @@
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Bom uso de recursos
 
-#Nunca deixar de descarregar pacotes de funções depois de utilizados para evitar conflitos com funções que tenham o nome,mas que sejam oriundas de 
-#outros pacotes e também por questões de redução de desempenho por sobrecarga da memória.
-
-#Também é conveniente Utilzar ao máximo funções otimizadas que utilizam a técnica da vetorização para ganho de eficiência.
+#Nunca deixar de descarregar pacotes de funções depois de utilizados para evitar conflitos com funções que tenham o mesmo nome, mas que pertençam a 
+#outros pacotes e também para não gerar redução de desempenho por sobrecarga da memória. Também é conveniente Utilzar ao máximo funções otimizadas 
+#que utilizam a técnica da vetorização para ganho de eficiência. A vetorização evita a utilização de laçõs de repetição na medida que as próprias 
+#funções já aplicam o procedimento para todos os valores de um vetor ou de uma matriz, por exemplo.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Organização de arquivos
 
-#Separar scripts por projeto, não por tema.
-
-#Não esquecer de salva o workspace para caso de precisar continuar o trabalho depois.
+#Separar scripts por projeto, não por tema. Dentro de um mesmo projeto podem residir vários scripts para fins diversos e que tenham relevância para
+#aquilo que se pretende com o projeto. Não esquecer de salva o workspace para caso de precisar continuar o trabalho depois.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Nomear funções com camel-case
 
-#Funções nomeadas com camel case são melhores para claramente distinguir o que é uma função e o que é uma variável comum.
+#Funções nomeadas com camel case são melhores para claramente distinguir o que é uma função e o que é uma variável comum. Assim, basta bater os olhos
+#para sabermos do que se trata.
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+#Indicar a origem das funções
+
+#Apesar de ser um pouco menos eficiente, comvém indicar o pacote de onde vem a função que está sendo utilizada com '::' principalmente quando nós 
+#utilizamos muitos pacotes. Isto nos permite identificar de que pacote é a função, além de que evita conflitos pois podemos ter funções de mesmo nome 
+#em pacotes distintos, mas não funções com mesmo nome dentro de um mesmo pacote.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Evitar usar a função attach()
 
-#Quando utilizada pode gerar muitas confusões. O ideal é sempre buscar o pacote quando ele for necessário.
+#Quando utilizada pode gerar muitas confusões. O ideal é sempre buscar o pacote quando ele for necessário. Neste aspecto, usar '::' para chamar a 
+#função diretamente de dentro do pacote pode vir a calhar.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 #Evitar atribuições da esquerda para a direita
@@ -75,13 +90,7 @@
 #Gerar retornos explícitos
 
 #Na criação de funções, R pode retornar os valores calculados automaticamente dentro das funções. Esse valores são aqueles que estiverem na última
-#linha do escopo da função. No entanto, é auspicioso retornar explicitamnete o valor que se deseja retornar utilizando a função return().
-
-#-----------------------------------------------------------------------------------------------------------------------------------------------------
-#Indicar a origem das funções
-
-#Apesar de ser um pouco menos eficiente, comvém indicar o pacote de onde vem a função com '::' principalmente quando utilizamos muitos pacotes. Isto
-#nos permite identificar de que pacote é a função, além de que evita conflitos pois podemos ter funções de mesmo nome em pacotes distintos, mas não
-#funções com mesmo nome dentro de um mesmo pacote.
+#linha do escopo da função. No entanto, é auspicioso retornar explicitamnete o valor que se deseja retornar utilizando a função return(). Isto deixa
+#claro para todos que lerem o código, o que se pretende com ele.
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
