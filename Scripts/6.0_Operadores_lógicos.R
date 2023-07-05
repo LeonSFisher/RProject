b43901c620047895f3073d2edd360525e7c1a636
Leandro Luz <leandroluzstat@gmail.com>
2023-07-05 00:09:25 -0300

    05/07/2023

--- Scripts/6.0_Operadores_lógicos.R
+++ Scripts/6.0_Operadores_lógicos.R
@@ -12,11 +12,11 @@
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #OPERADORES LÓGICOS USUAIS
   
-#Expressões lógicas servem para avaliarmos uma expressão como verdadeira ou falsa. Para isso, usamos os operadores lógicos 'E' e 'OU'. O operador E
-#tem duas formas que são & e &&. Já o operador OU tem as formas | ou ||. A diferença entre a forma simples e a forma dupla desses operadores está no
-#fato de que a forma dupla são operadores de curto-circuito. Isso quer dizer que, ao avaliar uma expressão, assim que um resultado é avaliado como
-#sendo falso, a comparação termina e o resultado é dado como falso, pois não há mais a necessidade de comparar o resto. Já a forma simples é chamada
-#de operador bit-a-bit (bitwise) que promove a comparação até o fim antes de retornar um valor falso ou verdadeiro. Caso a primeira avaliação que
+#Valores lógicas servem para avaliarmos uma expressão como verdadeira ou falsa. Para isso, usamos os operadores lógicos 'E' e 'OU'. O operador E tem 
+#duas formas que são & e &&. Já o operador OU tem as formas | ou ||. A diferença entre a forma simples e a forma dupla desses operadores está no fato 
+#de que a forma dupla são operadores de curto-circuito. Isso quer dizer que, ao avaliar uma expressão, assim que um resultado é avaliado como sendo 
+#falso, a comparação termina e o resultado é dado como falso, pois não há mais a necessidade de comparar o resto. Já a forma simples é chamada de 
+#operador bit-a-bit (bitwise) que promove a comparação até o fim antes de retornar um valor falso ou verdadeiro. Caso a primeira avaliação que
 #resulte num valo falso for a última a ser avaliada, os dois tipos de operadores, simples e duplo, serão equivalentes. Uma aplicação um pouco mais 
 #interessante será vista ao estudarmos vetores. Lá esta explicação fará mais sentido.
 
